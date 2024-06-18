b0f6ac39c40072531114fd51fd163aad042560fd
vickvey <vivek22@iiserb.ac.in>
2024-06-18 23:02:48 +0530

    learnt templates in Django

---
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, world. You are at chai aur Django Home Page.")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("Hello, world. You are at chai aur Django About Page.")
+
+def contact(request):
+    return HttpResponse("Hello, world. You are at chai aur Django Contact Page.")
